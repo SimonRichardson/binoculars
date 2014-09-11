9e5c13367806d1b04b8d48118716e3477254956d
Simon Richardson <stickupkid@gmail.com>
2014-09-11 18:12:33 +0100

    Multiple lenses

--- binoculars/lens.go
+++ binoculars/lens.go
@@ -1,5 +1,7 @@
 package binoculars
 
+import "reflect"
+
 type Lens struct {
 	Run func(Any) Store
 }
@@ -23,4 +25,44 @@ func (l Lens) Compose(x Lens) Lens {
 
 func (l Lens) AndThen(x Lens) Lens {
 	return x.Compose(l)
+}
+
+func (l Lens) And(x Lens) Lens {
+	return NewLens(func(target Any) Store {
+		a := l.Run(target)
+		b := x.Run(target)
+		return NewStore(
+			func(x Any) Any {
+				y := x.([]Any)
+				return merge(target, a.Set(y[0]), b.Set(y[1]))
+			},
+			b.Get,
+		)
+	})
+}
+
+func merge(a, b, c Any) Any {
+	src := reflect.ValueOf(a)
+
+	x := reflect.ValueOf(b)
+	y := reflect.ValueOf(c)
+
+	dst := reflect.New(src.Type()).Elem()
+	dst.Set(src)
+
+	for i := 0; i < dst.NumField(); i++ {
+		field := dst.Field(i)
+
+		poss_a := x.Field(i).Interface()
+		poss_b := y.Field(i).Interface()
+
+		orig := field.Interface()
+		if orig != poss_a {
+			field.Set(reflect.ValueOf(poss_a))
+		} else if orig != poss_b {
+			field.Set(reflect.ValueOf(poss_b))
+		}
+	}
+
+	return dst.Interface()
 }
